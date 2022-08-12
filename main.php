6b7079521c969b2047c2bb40ba598bb9017fbf47
dmitry-k-asb <dmitriy_k_asb@mail.ru>
2022-08-12 19:30:16 +0500

    3

--- main.php
+++ main.php
@@ -1,10 +1,10 @@
 <?php
 
-$access_token = 'vk1.a.DzdWvHdhlXu--x8QAgU1kE1b5J_BEotxEQCbd6QdXBZo5F3m4g4wKQ7_mJEBVSqPsx44lcJprhDaMiMwuwXffPMqyv2UUZ16rZfHWbv7arCKh1Hq7W4eab20o_vdlH_0QUW6NFNfzelHW9mCcixKwMP1cGKAN6moxuoIa4r2YrzqDNRjsJEp7Vwnyi7wbfvG';
-$group_id          = '215251375';
-$connection_string = 'mongodb://localhost:27017';
-$database          = 'qwe';
-$collection        = 'qwe';
+$access_token = 'введите токен';
+$group_id          = 'введите идентификатор группы';
+$connection_string = 'введите строку подключения к MongoDB';
+$database          = 'введите имя базы';
+$collection        = 'введите имя коллекции';
 
 require 'vkallmembersquery.php';
 require 'savetomongo.php';
