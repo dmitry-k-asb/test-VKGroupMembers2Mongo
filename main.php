d8ba1c5936af1ebcf7ca375e4d51c9556c0ee9c6
dmitry-k-asb <dmitriy_k_asb@mail.ru>
2022-08-16 16:58:24 +0500

    2022-08-16

--- main.php
+++ main.php
@@ -1,11 +1,20 @@
 <?php
 
-$access_token = 'введите токен';
-$group_id          = 'введите идентификатор группы';
-$connection_string = 'введите строку подключения к MongoDB';
-$database          = 'введите имя базы';
-$collection        = 'введите имя коллекции';
-
+if ($argc < 2 || is_null($argv[1]))
+{
+	exit('Не указан ID группы');
+}
+$group_id = $argv[1];
+$ini = parse_ini_file('config.ini');
+if (empty($ini))
+{
+	exit('Не удалось загрузить параметры из config.ini');
+}
+$access_token      = $ini['access_token'];
+$connection_string = $ini['connection_string'];
+$database_name     = $ini['database_name'];
+$collection_name   = $ini['collection_name'];
+var_dump($ini);
 require 'vkallmembersquery.php';
 require 'savetomongo.php';
 
@@ -14,9 +23,9 @@ $vk->query($access_token, $group_id);
 echo 'Всего собрано: ' . $vk->getCount() . PHP_EOL;
 $members = $vk->getCollectedItems();
 
-$mongo = new SaveToMongo($connection_string);
-$mongo->selectCollection($database, $collection);
-$mongo->saveData($members);
+// $mongo = new SaveToMongo($connection_string);
+// $mongo->selectCollection($database_name, $collection_name);
+// $mongo->saveData($members);
 
 
 ?>
